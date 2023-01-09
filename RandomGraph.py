b3322db01cf82f2a81765f1350d383bd966b0549
GuyTurevich <guy938@gmail.com>
2023-01-09 20:52:57 +0200

    changed to a singleton

--- RandomGraph.py
+++ RandomGraph.py
@@ -2,12 +2,26 @@ import random
 from Graph import Graph
 
 class RandomGraph:
-    numOfVertices = 20
-    inputGraph = Graph(numOfVertices)
-    for i in range(numOfVertices):
-        for j in range(i+1,numOfVertices):
-            if random.uniform(0,1) >= 0.3:    # 70% chance of being connected
-                inputGraph.add_neighbours(i , j)
+    __instance = None  #singleton instance
+
+
+    def __init__(self,num_of_vertices : int):
+        if RandomGraph.__instance != None:
+            raise Exception("Singleton already exists, use get_instance() method")
+        else:
+            self.num_of_vertices = num_of_vertices
+            self.inputGraph = Graph(num_of_vertices)
+            for i in range(num_of_vertices):
+                for j in range(i+1,num_of_vertices):
+                    if random.uniform(0,1) >= 0.3:              # 70% chance of being connected
+                        self.inputGraph.add_neighbours(i , j)
+            RandomGraph.__instance = self                       # singleton instance created
+
+    @classmethod
+    def get_instance(cls):
+        if cls.__instance == None:
+            raise Exception("Singleton was not created, use constructor")
+        return cls.__instance
 
     def get_graph(self):
         return self.inputGraph
